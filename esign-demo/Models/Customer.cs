119b9944dc432afefea0b273dda3e8642357ce82
Anthony Cordero <tony170593@gmail.com>
2024-05-25 18:18:42 -0600

    Iniciating Proyect Esing-demo

---
+++ esign-demo/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace esign-demo.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        // Otros campos necesarios
+    }
+}
