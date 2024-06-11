363a6e96b54b3b272793bfa134c4b2a62fe7b196
Anthony Cordero <tony170593@gmail.com>
2024-06-10 20:15:05 -0600

    First Bug From: Exception has occurred: CLR/System.ArgumentException An unhandled exception of type 'System.ArgumentException' occurred in Microsoft.EntityFrameworkCore.dll: ''AddDbContext' was called with configuration, but the context type 'AppDbContext' only declares a parameterless constructor. This means that the configuration passed to 'AddDbContext' will never be used. If configuration is passed to 'AddDbContext', then 'AppDbContext' should declare a constructor that accepts a DbContextOptions<AppDbContext> and must pass it to the base constructor for DbContext.'

--- esignDemo/Program.cs
+++ esignDemo/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using esignDemo.Data;
-using esignDemo.Services;
 
 namespace esignDemo
 {
-    class Program
+    public class Program
     {
-                public static void Main(string[] args)
+        public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
@@ -22,36 +16,5 @@ namespace esignDemo
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        // static async Task Main(string[] args)
-        // {
-        //     using (var db = new AppDbContext())
-        //     {
-        //         var customer = db.Customers.FirstOrDefault();
-
-        //         if (customer == null)
-        //         {
-        //             Console.WriteLine("No se encontró ningún cliente.");
-        //             return;
-        //         }
-
-        //         var documentGenerator = new DocumentGenerator();
-        //         string documentPath = documentGenerator.GenerateDocument(customer);
-
-        //         var signNowService = new SignNowService();
-
-        //         string accessToken = await signNowService.GetAccessToken();
-
-        //         if (!string.IsNullOrEmpty(accessToken))
-        //         {
-        //             string documentId = await signNowService.UploadDocument(accessToken, documentPath);
-
-        //             if (!string.IsNullOrEmpty(documentId))
-        //             {
-        //                 await signNowService.SendDocumentForSignature(accessToken, documentId, customer.Email, customer.Name);
-        //                 Console.WriteLine("Documento enviado para firma.");
-        //             }
-        //         }
-        //     }
-        // }
     }
 }
